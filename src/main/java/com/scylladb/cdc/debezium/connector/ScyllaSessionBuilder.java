3b5fa906afa9403918c810296c83c912345a0ef9
Joseph Perez <joperez@hotmail.fr>
2023-01-23 09:02:05 +0100

    Try to add ssl

--- src/main/java/com/scylladb/cdc/debezium/connector/ScyllaSessionBuilder.java
+++ src/main/java/com/scylladb/cdc/debezium/connector/ScyllaSessionBuilder.java
@@ -1,6 +1,7 @@
 package com.scylladb.cdc.debezium.connector;
 
 import com.scylladb.cdc.cql.CQLConfiguration;
+import com.scylladb.cdc.cql.SslConfig;
 import com.scylladb.cdc.cql.driver3.Driver3Session;
 
 public class ScyllaSessionBuilder {
@@ -19,6 +20,20 @@ public class ScyllaSessionBuilder {
         builder.withConsistencyLevel(configuration.getConsistencyLevel());
         if (configuration.getLocalDCName() != null) {
             builder.withLocalDCName(configuration.getLocalDCName());
+        }
+        if (configuration.getSslEnabled()) {
+            SslConfig.Builder sslBuilder = SslConfig.builder();
+            sslBuilder.withSslProviderString(configuration.getSslProvider().toString());
+            sslBuilder.withTrustStorePath(configuration.getTrustStorePath());
+            sslBuilder.withTrustStorePassword(configuration.getTrustStorePassword());
+            sslBuilder.withKeyStorePath(configuration.getKeyStorePath());
+            sslBuilder.withKeyStorePassword(configuration.getKeyStorePassword());
+            if (configuration.getCipherSuite() != null) {
+                configuration.getCipherSuite().stream().forEach(sslBuilder::withCipher);
+            }
+            sslBuilder.withCertPath(configuration.getCertPath());
+            sslBuilder.withPrivateKeyPath(configuration.getPrivateKeyPath());
+            builder.withSslConfig(sslBuilder.build());
         }
         return new Driver3Session(builder.build());
     }
